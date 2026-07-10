60f71fbf8b11719b3478bba71cf0f5da453e2014
Rainly8023 <1959516061@qq.com>
2026-07-10 20:59:38 +0800

    fix: update service worker cache strategy to network first

--- sw.js
+++ sw.js
@@ -1,35 +1,40 @@
-const CACHE_NAME = 'rainly-cache-v1';
+const CACHE_NAME = 'rainly-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
   '/about.html',
   '/blog.html',
   '/gallery.html',
-  '/css/kawaii.css',
-  '/js/init.js',
-  '/js/mouse-trail.js',
-  '/js/sakura-petals.js',
-  '/js/click-heart.js',
-  '/js/scroll-reveal.js',
-  '/js/gallery-3d.js',
-  '/js/blog-render.js'
+  '/css/kawaii.css'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Network First strategy
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
